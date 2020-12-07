a60640959b6d4e1d3fcb2fe4913b777363135087
ariel_rio_sie <ariel.rios@studioav.com.mx>
2020-12-07 16:50:12 -0600

    [INITIAL] Inicio de la libreria del ADE9000

---
+++ include/ADE9000.h
@@ -0,0 +1,20 @@
+/*
+  ADE9000API.h - Library based in ADE9000 - Energy and PQ monitoring AFE
+  This library have with base the library of nchandra development by Analog Devices
+  for compatibility at ESP32
+  Author: Ariel Rios
+  Date: 12-07-2020
+*/
+#ifndef ADE9000_H
+#define ADE9000
+
+
+/****************************************************************************************************************
+ Includes
+***************************************************************************************************************/
+
+#include "Arduino.h"
+#include  "ADE9000RegMap.h"
+
+
+#endif
